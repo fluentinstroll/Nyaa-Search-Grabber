9dc944afeec852363a15b67bf310b49578915254
fluentinstroll <fluentinstroll@protonmail.com>
2021-01-09 22:03:53 -0500

    addec content to README and added command line arguments

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # nyaa-anime-grabber
 An npm script to grab torrents from nyaa rss feeds.
 
+## To Use
+
+1. clone this repo
+2.  `npm install` inside the cloned repo
+3. `npm run nyaagrab '<name of search target>' '<number of results to get>'`
+
 Uses [Nyaapi](https://github.com/Kylart/Nyaapi) to search.
--- index.js
+++ index.js
@@ -3,16 +3,28 @@ const { si } = require('nyaapi');
 /**********************************************************************************
  * API info: https://github.com/Kylart/Nyaapi/wiki/Nyaa.si#search
  * TODO: 
- * - args to specify the search term
  * - pump links into text file(?)
  * - open links in torrent software(? Maybe this needs to be a separate script) 
  *********************************************************************************/
-let searchTerm = 'dr stone';
-const limit = 10;
+
+var limit;
+var searchTerm;
+
+if(!process.argv[2]){
+    console.log('please enter a search term')
+} else { 
+    if(!process.argv[3] || isNaN(process.argv[3])){
+        limit = 10;
+    } else {
+        limit = parseInt(process.argv[3]);
+    }
+    searchTerm = process.argv[2];
+}
 
 si.searchAll(searchTerm, {filter: 2, })
-.then((data) => 
-    data.forEach(data => {
-        console.log(`name: ${data.name} link: ${data.torrent}`);
+.then((data) =>
+    data.forEach((data, index) => {
+        if(index < limit)
+            console.log(`name: ${data.name} link: ${data.torrent}`);
     }))
 .catch((err) => console.log(err));
