21c09f879b6c47ae500df02eec461be2f39935ac
fluentinstroll <fluentinstroll@protonmail.com>
2021-01-13 12:52:59 -0500

    added ability to search for english

--- index.js
+++ index.js
@@ -13,19 +13,23 @@ if (!process.argv[2]) {
   console.log("please enter a search term");
 } else {
   let searchTerm = process.argv[2];
+  let aCategory;
+  if (process.argv[4] == "-e") {
+    aCategory = "1_2";
+  }
 
   /* searchAll settings
    *  filter 0 = no filter
    *  filter 1 = no remakes
    *  filter 2 = trusted only
-   * *********
+   * -------------------------
    * no category = 'All categories'
    * category 1_1 = amv
    * category 1_2 = english sub
    */
-  si.searchAll(searchTerm, { filter: 2 })
+  si.searchAll(searchTerm, { filter: 2, category: aCategory })
     .then((data, limit) => {
-      if (process.argv[3]) {
+      if (process.argv[3] && parseInt(process.argv[3]) <= 100) {
         limit = parseInt(process.argv[3]);
       } else {
         limit = 10;
