854752ecbb04f9323d4af0a20d75c2fdb6172a1b
fluentinstroll <fluentinstroll@protonmail.com>
2021-01-13 12:21:27 -0500

    removed global variables

--- index.js
+++ index.js
@@ -9,35 +9,32 @@ const fs = require("fs");
  * - add ability to get only english sub
  *********************************************************************************/
 
-var limit;
-var searchTerm;
-
 if (!process.argv[2]) {
   console.log("please enter a search term");
 } else {
-  if (!process.argv[3] || isNaN(process.argv[3])) {
-    limit = 10;
-  } else {
-    limit = parseInt(process.argv[3]);
-  }
-  searchTerm = process.argv[2];
+  let searchTerm = process.argv[2];
 
   /* searchAll settings
-  *  filter 0 = no filter
-  *  filter 1 = no remakes
-  *  filter 2 = trusted only
-  * *********
-  * no category = 'All categories'
-  * category 1_1 = amv
-  * category 1_2 = english sub
-  */
-  si.searchAll(searchTerm, { filter: 2, })
-    .then((data) =>
+   *  filter 0 = no filter
+   *  filter 1 = no remakes
+   *  filter 2 = trusted only
+   * *********
+   * no category = 'All categories'
+   * category 1_1 = amv
+   * category 1_2 = english sub
+   */
+  si.searchAll(searchTerm, { filter: 2 })
+    .then((data, limit) => {
+      if (process.argv[3]) {
+        limit = parseInt(process.argv[3]);
+      } else {
+        limit = 10;
+      }
       data.forEach((data, index) => {
         if (index < limit)
           console.log(`name: ${data.name} link: ${data.torrent}`);
-      })
-    )
+      });
+    })
     .catch((err) => {
       console.log(
         "Could not connect to https://nyaa.si/, is the website down?"
